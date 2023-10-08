6f7830f587893d228f6c077b7a4dd14523c47ae2
Lawrence <lawrencehome21@gmail.com>
2023-10-07 18:30:01 -0700

    syntax changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Hello from './components/Hello';
 import ClassHello from './components/ClassHello';
 function App() {
   return (
     <div className="App">
-      <Hello/>
       <ClassHello/>
     </div>
   );
