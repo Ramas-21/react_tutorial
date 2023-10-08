5ee710e1b2d6672e6fdf33915e5c4beac2c6a786
Lawrence <lawrencehome21@gmail.com>
2023-10-07 20:56:38 -0700

    button handling

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,30 @@
 // STATE = STATE IS PREFERRED BECAUSE IT HOLDS INFORMATION AND THE INFORMATION MAY CHANGE OVER THE LIFETIME OF THAT COMPONENT COMPARED TO PROPS
 import { Component } from "react";
 class Counter extends Component {
-    constructor (){
-        super();
-        this.state = {
-            counter: 0,
-        }
-    }
-    render (){
-        return (
-        <div>
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+  increment() {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+  decrement() {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  }
+  render() {
+    return (
+      <div>
         <h1>Count value is: {this.state.counter}</h1>
-        <button>Click</button>
-        </div>
-        );
-    }
+        <button onClick={() => this.increment()}>Click</button>
+        <button onClick={() => this.decrement()}>Click</button>
+      </div>
+    );
+  }
 }
 export default Counter;
