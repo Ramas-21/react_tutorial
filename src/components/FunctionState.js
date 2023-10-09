dd016c4ef23a5457e1768910f183e93b1d144ffc
Lawrence <lawrencehome21@gmail.com>
2023-10-09 14:29:09 -0700

    decrementing the counter using state

--- src/components/FunctionState.js
+++ src/components/FunctionState.js
@@ -3,6 +3,9 @@ function FunctionState(){
 const [counter, setCounter] = useState(0);
 const increment = () =>{
     setCounter(counter + 1);
+};
+const decrement = () =>{
+    setCounter(counter - 1);
 }
 return(
     <div>
@@ -11,6 +14,7 @@ return(
         </div>
         <div>
             <button onClick = {increment}>Increment</button>
+            <button onClick = {decrement}>Decrement</button>
         </div>
     </div>
 
