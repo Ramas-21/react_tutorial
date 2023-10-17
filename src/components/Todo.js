a044703921aa44978c4cf84172b75dae13d2b747
Lawrence <lawrencehome21@gmail.com>
2023-10-17 10:06:15 -0700

    handled the user input

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,12 +2,23 @@ import { useState } from "react";
 
 function Todo(){
     const [todo, setTodo] = useState("");
+    const [todoList, setTodoList] = useState([]);
+
     const handleChange = (event) =>{
         setTodo(event.target.value);
     }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        let tempList = todoList;
+        tempList.push(todo);
+        setTodoList(tempList);
+        setTodo("");
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input value={todo} onChange={handleChange} type="text"></input>
                 <button type="submit">Add</button>
             </form>
