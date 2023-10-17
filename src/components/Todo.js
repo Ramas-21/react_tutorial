f0e89840818dba945a83fc7daff6549148934f15
Lawrence <lawrencehome21@gmail.com>
2023-10-17 09:51:01 -0700

    created a form

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,17 @@
 import { useState } from "react";
 
 function Todo(){
-    const [todo, setTodo] = useState();
-    return <div>Todo</div>
+    const [todo, setTodo] = useState("");
+    const handleChange = (event) =>{
+        setTodo(event.target.value);
+    }
+    return (
+        <div>
+            <form>
+                <input value={todo} onChange={handleChange} type="text"></input>
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    )
 }
 export default Todo;
